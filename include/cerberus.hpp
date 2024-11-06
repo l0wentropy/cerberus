d68e0983a050af561d8688d409a245a6c418b978
kernelp4n1c <l0wentropy@proton.me>
2024-11-06 15:36:28 +0000

    initial commit

---
+++ include/cerberus.hpp
@@ -0,0 +1,117 @@
+#ifndef __CERBERUS_HPP
+#define __CERBERUS_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include "utils.hpp"
+#include "config.hpp"
+
+#define _MAX_KEY_FILE_SIZE          1024 * 1024 * 64
+#define PBKDF2_PRE_ITERATIONS       1024
+#define ARGON2ID_VAR                0x00
+#define ARGON2I_VAR                 0x01
+#define ARGON2D_VAR                 0x02
+#define ARGON2_DEFAULT_ITERATIONS   16
+#define ARGON2_DEFAULT_THREADS      4
+#define ARGON2_DEFAULT_MEM_DEGREE   20
+
+/* Metadata structure
+*   ...encrypted data...
+*   RSA payload (key, iv, tag) [RSA]
+*   RSA payload data size (2 bytes) [RSA]
+*   Plain AES verification tag [Argon2]
+*   Argon2 variant (id, i, d, 1 byte) [Argon2]
+*   Argon2 iterations (4 bytes) [Argon2]
+*   Argon2 threads (4 bytes) [Argon2]
+*   Argon2 memory degree (1 byte) [Argon2]
+*   Key management opcode (RSA/Argon2/ECC, 1 byte) [General]
+*   AES verification tag opcode (attached/detached, 1 byte) [General]
+*   AES signature (8 byte)
+*/
+const unsigned int AES_SIGNATURE_SIZE = 8;
+const unsigned char AES_FILE_SIGNATURE[AES_SIGNATURE_SIZE] = { 0x3a, 0x7f, 0x7c, 0x72, 0xbc, 0x94, 0xae, 0xda };
+
+const unsigned char AES_KEY_OPCODE_SIZE = 0x01;
+const unsigned char AES_TAG_OPCODE_SIZE = 0x01;
+const unsigned char AES_ENC_OPT_RSA = 0x00;
+const unsigned char AES_ENC_OPT_KEY = 0x01;
+const unsigned char AES_ENC_OPT_ECC = 0x02;
+const unsigned char AES_ENC_OPT_TAG_ATTACHED = 0x00;
+const unsigned char AES_ENC_OPT_TAG_DETACHED = 0x01;
+
+const unsigned int RSA_METADATA_SIZE = 2;
+const unsigned int ARGON2_METADATA_SIZE = 1 + 4 + 4 + 1;
+
+/*
+#define GCM_PROCESS_CHUNK_SIZE  1024 * 8
+
+unsigned char AAD_DATA[] = {
+  0x30, 0x37, 0x44, 0x34, 0x35, 0x34, 0x31, 0x30, 0x45, 0x36, 0x43, 0x30,
+  0x38, 0x45, 0x39, 0x41, 0x30, 0x38, 0x41, 0x44, 0x38, 0x34, 0x37, 0x38,
+  0x30, 0x39, 0x44, 0x42, 0x42, 0x31, 0x45, 0x38, 0x39, 0x33, 0x41, 0x39,
+  0x34, 0x30, 0x35, 0x38, 0x44, 0x45, 0x31, 0x33, 0x36, 0x43, 0x34, 0x43,
+  0x34, 0x30, 0x42, 0x33, 0x32, 0x35, 0x35, 0x42, 0x35, 0x35, 0x35, 0x45,
+  0x32, 0x46, 0x38, 0x38, 0x37, 0x30, 0x31, 0x41, 0x38, 0x41, 0x44, 0x36,
+  0x34, 0x41, 0x35, 0x33, 0x35, 0x31, 0x38, 0x44, 0x32, 0x41, 0x39, 0x41,
+  0x33, 0x33, 0x46, 0x34, 0x45, 0x43, 0x31, 0x36, 0x45, 0x46, 0x42, 0x34,
+  0x32, 0x46, 0x33, 0x34, 0x30, 0x42, 0x44, 0x33, 0x38, 0x35, 0x36, 0x39,
+  0x45, 0x46, 0x31, 0x35, 0x44, 0x33, 0x35, 0x37, 0x37, 0x39, 0x46, 0x31,
+  0x42, 0x42, 0x45, 0x34, 0x35, 0x42, 0x31, 0x31
+};
+const unsigned int AAD_DATA_len = 128;
+
+*/
+
+class Cerberus
+{
+public:
+  Cerberus();
+  Cerberus(const std::string &_sIn, const std::string &_sOut);
+
+  bool encryptFile();
+  bool decryptFile();
+  void processWithRsa();
+  void processWithKeys();
+  void setInOutPaths(const std::string &_sIn, const std::string &_sOut);
+  void setRsaKey(RSA *_rsaKey);
+  void setPassphrase(const std::vector<unsigned char> &_vPassphraseBytes);
+  void setKeyFileData(const std::vector<unsigned char> &_vKeyFileBytes);
+  void unsetRsaKey();
+  void unsetPassphrase();
+  void unsetKeyFileData();
+  void setArgonParams(const unsigned char &_ucVariant, const unsigned int &_uiIterations, const unsigned int &_uiThreads, const unsigned char &_ucMemory);
+  void detachTag(const std::string &_sTag);
+  void attachTag();
+  void setForce();
+  void unsetForce();
+
+  ~Cerberus();
+
+private:
+  bool bProcessWithRsa = true;
+  bool bDetachHeader = false;
+  bool bForce = false;
+
+  std::string sIn;
+  std::string sOut;
+  std::string sTag;
+
+  std::vector<unsigned char> vPassphraseBytes, vKeyFileBytes;
+  RSA *rsaKey = NULL;
+  unsigned char ucArgonVariant = ARGON2ID_VAR;
+  unsigned int uiArgonIterations = ARGON2_DEFAULT_ITERATIONS;
+  unsigned int uiArgonThreads = ARGON2_DEFAULT_ITERATIONS;
+  unsigned char ucArgonMemory = ARGON2_DEFAULT_MEM_DEGREE;
+
+  void reset();
+  bool _encryptFile();
+  bool _decryptFile();
+  bool deriveAesKeys(const std::vector<unsigned char> &_vPassphraseBytes, const std::vector<unsigned char> &_vKeyFileBytes, std::vector<unsigned char> &_vKey, std::vector<unsigned char> &_vIv);
+};
+
+#endif
---
+++ src/cerberus.cpp
@@ -0,0 +1,652 @@
+#include "cerberus.hpp"
+
+Cerberus::Cerberus()
+{
+
+}
+
+Cerberus::Cerberus(const std::string &_sIn, const std::string &_sOut)
+{
+  sIn = _sIn;
+  sOut = _sOut;
+}
+
+bool Cerberus::encryptFile()
+{
+  if (sIn.empty() || sOut.empty())
+  {
+    printf("Input and output files paths must be set first\n");
+    return false;
+  }
+  if (bProcessWithRsa && rsaKey == NULL)
+  {
+    printf("RSA key is not loaded\n");
+    return false;
+  }
+  else if (!bProcessWithRsa && vPassphraseBytes.empty() && vKeyFileBytes.empty())
+  {
+    printf("Either passphrase or key file data must be loaded\n");
+    return false;
+  }
+
+  return _encryptFile();
+}
+
+bool Cerberus::decryptFile()
+{
+  if (sIn.empty() || sOut.empty())
+  {
+    printf("Input and output files paths must be set first\n");
+    return false;
+  }
+  if (bProcessWithRsa && rsaKey == NULL)
+  {
+    printf("RSA key is not loaded\n");
+    return false;
+  }
+  else if (!bProcessWithRsa && vPassphraseBytes.empty() && vKeyFileBytes.empty())
+  {
+    printf("Either passphrase or key file data must be loaded\n");
+    return false;
+  }
+
+  return _decryptFile();
+}
+
+void Cerberus::processWithRsa()
+{
+  bProcessWithRsa = true;
+}
+
+void Cerberus::processWithKeys()
+{
+  bProcessWithRsa = false;
+}
+
+void Cerberus::setInOutPaths(const std::string &_sIn, const std::string &_sOut)
+{
+  sIn = _sIn;
+  sOut = _sOut;
+}
+
+void Cerberus::setRsaKey(RSA *_rsaKey)
+{
+  rsaKey = _rsaKey;
+  bProcessWithRsa = true;
+}
+
+void Cerberus::setPassphrase(const std::vector<unsigned char> &_vPassphraseBytes)
+{
+  vPassphraseBytes = _vPassphraseBytes;
+  bProcessWithRsa = false;
+}
+
+void Cerberus::setKeyFileData(const std::vector<unsigned char> &_vKeyFileBytes)
+{
+  vKeyFileBytes = _vKeyFileBytes;
+  bProcessWithRsa = false;
+}
+
+void Cerberus::unsetRsaKey()
+{
+  rsaKey = NULL;
+  bProcessWithRsa = false;
+}
+
+void Cerberus::unsetPassphrase()
+{
+  vPassphraseBytes.clear();
+}
+
+void Cerberus::unsetKeyFileData()
+{
+  vKeyFileBytes.clear();
+}
+
+void Cerberus::setArgonParams(const unsigned char &_ucVariant, const unsigned int &_uiIterations, const unsigned int &_uiThreads, const unsigned char &_ucMemory)
+{
+  ucArgonVariant = _ucVariant;
+  uiArgonIterations = _uiIterations;
+  uiArgonThreads = _uiThreads;
+  ucArgonMemory = _ucMemory;
+}
+
+void Cerberus::detachTag(const std::string &_sTag)
+{
+  sTag = _sTag;
+  bDetachHeader = true;
+}
+
+void Cerberus::attachTag()
+{
+  sTag.clear();
+  bDetachHeader = false;
+}
+
+void Cerberus::setForce()
+{
+  bForce = true;
+}
+
+void Cerberus::unsetForce()
+{
+  bForce = false;
+}
+
+Cerberus::~Cerberus()
+{
+
+}
+
+void Cerberus::reset()
+{
+  bProcessWithRsa = true;
+  bDetachHeader = false;
+  bForce = false;
+  rsaKey = NULL;
+  sIn.clear();
+  sOut.clear();
+  sTag.clear();
+  vPassphraseBytes.clear();
+  vKeyFileBytes.clear();
+  ucArgonVariant = ARGON2ID_VAR;
+  uiArgonIterations = ARGON2_DEFAULT_ITERATIONS;
+  uiArgonThreads = ARGON2_DEFAULT_ITERATIONS;
+  ucArgonMemory = ARGON2_DEFAULT_MEM_DEGREE;
+}
+
+bool Cerberus::_encryptFile()
+{
+  unsigned long long ullProcessedBytes = 0;
+  std::vector<unsigned char> vKey(AES256_KEY_SIZE, 0), vIv(IV_SIZE, 0), vTag(TAG_SIZE, 0);
+  std::vector<unsigned char> vAppend;
+  const mode_t flagsWrite = O_WRONLY | O_APPEND;
+
+  if (bProcessWithRsa)
+  {
+    if (!utils::genRandBytes(vKey, vKey.size()) || !utils::genRandBytes(vIv, vIv.size()))
+    {
+      printf("Cannot generate random bytes for AES key or/and IV\n");
+      return false;
+    }
+  }
+  else
+  {
+    printf("Argon2 options:");
+    printf("\n\t");
+    // TODO: change default behaviour. check when setting params 
+    if (ucArgonVariant == ARGON2ID_VAR)
+    {
+      printf("Variant [Argon2id]");
+    }
+    else if (ucArgonVariant == ARGON2I_VAR)
+    {
+      printf("Variant [Argon2i]");
+    }
+    else
+    {
+      printf("Variant [Argon2d]");
+    }
+    printf("\n\t");
+    printf("Iterations [%d]", uiArgonIterations);
+    printf("\n\t");
+    printf("Threads [%d]", uiArgonThreads);
+    printf("\n\t");
+    printf("Memory [%llu KiB]\n", (unsigned long long)((unsigned long long)(1 << ucArgonMemory) * 1024) / 1024);
+
+    printf("Deriving keys...\n");
+    if (!deriveAesKeys(vPassphraseBytes, vKeyFileBytes, vKey, vIv))
+    {
+      return false;
+    }
+  }
+
+  printf("Encryption...\n");
+  if (!utils::gcmEncryptFileWrap(sIn, sOut, vKey, vIv, ullProcessedBytes, vTag))
+  {
+    printf("Error occurred during encryption\n");
+    return false;
+  }
+
+  if (bProcessWithRsa)
+  {
+    const unsigned int rsaBuf = RSA_size(rsaKey);
+    std::vector<unsigned char> vBufRSA(rsaBuf);
+
+    vAppend.insert(vAppend.end(), vKey.begin(), vKey.end());
+    vAppend.insert(vAppend.end(), vIv.begin(), vIv.end());
+    if (!bDetachHeader)
+    {
+      vAppend.insert(vAppend.end(), vTag.begin(), vTag.end());
+    }
+
+    printf("RSA encryption...\n");
+    const int rsaRetval = utils::encryptRsa(vAppend.size(), (unsigned char*)vAppend.data(), &vBufRSA[0], rsaKey);
+
+    if (rsaRetval <= 0)
+    {
+      printf("RSA encryption failed\n");
+      return false;
+    }
+
+    vAppend.clear();
+
+    vAppend.insert(vAppend.end(), vBufRSA.begin(), vBufRSA.begin() + rsaRetval);
+
+    vAppend.push_back((rsaRetval >> 8) & 0xff);
+    vAppend.push_back(rsaRetval & 0xff);
+
+    vAppend.push_back(AES_ENC_OPT_RSA);
+  }
+  else
+  {
+    if (!bDetachHeader)
+    {
+      vAppend.insert(vAppend.end(), vTag.begin(), vTag.end());
+    }
+    vAppend.push_back(ucArgonVariant);
+
+    vAppend.push_back((uiArgonIterations >> 24) & 0xff);
+    vAppend.push_back((uiArgonIterations >> 16) & 0xff);
+    vAppend.push_back((uiArgonIterations >> 8) & 0xff);
+    vAppend.push_back(uiArgonIterations & 0xff);
+
+    vAppend.push_back((uiArgonThreads >> 24) & 0xff);
+    vAppend.push_back((uiArgonThreads >> 16) & 0xff);
+    vAppend.push_back((uiArgonThreads >> 8) & 0xff);
+    vAppend.push_back(uiArgonThreads & 0xff);
+
+    vAppend.push_back(ucArgonMemory);
+
+    vAppend.push_back(AES_ENC_OPT_KEY);
+  }
+
+  if (bDetachHeader)
+  {
+    if (!utils::WriteFile(sTag, vTag, O_CREAT | O_WRONLY))
+    {
+      printf("Could not write detached verification tag to file [%s]\n", sTag.c_str());
+      return false;
+    }
+
+    vAppend.push_back(AES_ENC_OPT_TAG_DETACHED);
+  }
+  else
+  {
+    vAppend.push_back(AES_ENC_OPT_TAG_ATTACHED);
+  }
+
+  for (unsigned int i = 0; i < AES_SIGNATURE_SIZE; ++i)
+  {
+    vAppend.push_back(AES_FILE_SIGNATURE[i]);
+  }
+
+  if (!utils::WriteFile(sOut, vAppend, flagsWrite))
+  {
+    printf("Could not append metadata to encrypted file\n");
+    return false;
+  }
+
+  return true;
+}
+
+bool Cerberus::_decryptFile()
+{
+  int fdIn = 0;
+  const mode_t flagsRead = O_RDONLY | O_NOATIME;
+  long long llFileSize = 0;
+  long long llReadLimit = 0;
+  unsigned long long ullProcessedBytes = 0;
+  unsigned short usRsaPayloadSize = 0;
+  bool bIsTagDetached = false;
+  bool bIsEncryptedWithRsa = true;
+  std::vector<unsigned char> vKey, vIv, vAppend, vTmp, vTag;
+
+  fdIn = utils::OpenFile(sIn, flagsRead, llFileSize);
+  if (fdIn == -1)
+  {
+    printf("Error open input file [%s]\n", sIn.c_str());
+    return false;
+  }
+  if (llFileSize <= AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE)
+  {
+    printf("Input file seems to be corrupted [%s]\n", sIn.c_str());
+    utils::CloseFile(fdIn);
+    return false;
+  }
+
+  vTmp.resize(AES_SIGNATURE_SIZE, 0);
+  utils::SetSeekFileOffset(fdIn, llFileSize - AES_SIGNATURE_SIZE);
+
+  if (utils::ReadFileChunk(fdIn, AES_SIGNATURE_SIZE, vTmp) != AES_SIGNATURE_SIZE)
+  {
+    printf("Error read signature [%s]\n", sIn.c_str());
+    utils::CloseFile(fdIn);
+    return false;
+  }
+
+  for (unsigned int i = 0; i < AES_SIGNATURE_SIZE; ++i)
+  {
+    if (AES_FILE_SIGNATURE[i] != vTmp[i])
+    {
+      printf("Signature is not found [%s]\n", sIn.c_str());
+      utils::CloseFile(fdIn);
+      return false;
+    }
+  }
+
+  vTmp.resize(AES_TAG_OPCODE_SIZE, 0);
+  utils::SetSeekFileOffset(fdIn, llFileSize - (AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE));
+
+  if (utils::ReadFileChunk(fdIn, AES_TAG_OPCODE_SIZE, vTmp) != AES_TAG_OPCODE_SIZE)
+  {
+    printf("Error read tag metadata opcode [%s]\n", sIn.c_str());
+    utils::CloseFile(fdIn);
+    return false;
+  }
+
+  if (vTmp[0] != AES_ENC_OPT_TAG_DETACHED && vTmp[0] != AES_ENC_OPT_TAG_ATTACHED)
+  {
+    printf("Tag metadata opcode is invalid\n");
+    utils::CloseFile(fdIn);
+    return false;
+  }
+
+  bIsTagDetached = vTmp[0] == AES_ENC_OPT_TAG_DETACHED ? true : false;
+
+  vTmp.resize(AES_KEY_OPCODE_SIZE, 0);
+  utils::SetSeekFileOffset(fdIn, llFileSize - (AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE));
+
+  if (utils::ReadFileChunk(fdIn, AES_KEY_OPCODE_SIZE, vTmp) != AES_KEY_OPCODE_SIZE)
+  {
+    printf("Error read key management metadata opcode [%s]\n", sIn.c_str());
+    utils::CloseFile(fdIn);
+    return false;
+  }
+
+  if (vTmp[0] != AES_ENC_OPT_KEY && vTmp[0] != AES_ENC_OPT_RSA)
+  {
+    printf("Key management metadata opcode is invalid\n");
+    utils::CloseFile(fdIn);
+    return false;
+  }
+
+  bIsEncryptedWithRsa = vTmp[0] == AES_ENC_OPT_RSA ? true : false;
+
+  if (bIsEncryptedWithRsa)
+  {
+    if (rsaKey == NULL)
+    {
+      printf("RSA key is not loaded\n");
+      utils::CloseFile(fdIn);
+      return false;
+    }
+    if (llFileSize <= RSA_METADATA_SIZE + AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE)
+    {
+      printf("Input file seems to be corrupted [%s]\n", sIn.c_str());
+      utils::CloseFile(fdIn);
+      return false;
+    }
+
+    vTmp.resize(RSA_METADATA_SIZE, 0);
+    utils::SetSeekFileOffset(fdIn, llFileSize - (RSA_METADATA_SIZE + AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE));
+
+    if (utils::ReadFileChunk(fdIn, RSA_METADATA_SIZE, vTmp) != RSA_METADATA_SIZE)
+    {
+      printf("Error read RSA metadata [%s]\n", sIn.c_str());
+      utils::CloseFile(fdIn);
+      return false;
+    }
+
+    usRsaPayloadSize = vTmp[0];
+    usRsaPayloadSize <<= 8;
+    usRsaPayloadSize |= vTmp[1];
+
+    if (llFileSize <= usRsaPayloadSize + RSA_METADATA_SIZE + AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE)
+    {
+      printf("Input file seems to be corrupted [%s]\n", sIn.c_str());
+      utils::CloseFile(fdIn);
+      return false;
+    }
+
+    vTmp.resize(usRsaPayloadSize, 0);
+    llReadLimit = llFileSize - (usRsaPayloadSize + RSA_METADATA_SIZE + AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE);
+    utils::SetSeekFileOffset(fdIn, llReadLimit);
+
+    if (utils::ReadFileChunk(fdIn, usRsaPayloadSize, vTmp) != usRsaPayloadSize)
+    {
+      printf("Error read RSA payload [%s]\n", sIn.c_str());
+      utils::CloseFile(fdIn);
+      return false;
+    }
+
+    const unsigned int rsaBuf = RSA_size(rsaKey);
+    const unsigned int payloadTagSize = bIsTagDetached ? 0 : TAG_SIZE;
+    const std::vector<unsigned char> vRsaData = vTmp;
+    vTmp.clear();
+    vTmp.resize(rsaBuf, 0);
+
+    printf("RSA decryption...\n");
+    const int rsaRetval = utils::decryptRsa(vRsaData.size(), (unsigned char*)vRsaData.data(), &vTmp[0], rsaKey);
+
+    if (rsaRetval != AES256_KEY_SIZE + IV_SIZE + payloadTagSize)
+    {
+      printf("Error decrypt RSA payload [%s]\n", sIn.c_str());
+      utils::CloseFile(fdIn);
+      return false;
+    }
+
+    vTmp.resize(rsaRetval);
+    vKey.insert(vKey.end(), vTmp.begin(), vTmp.begin() + AES256_KEY_SIZE);
+    vIv.insert(vIv.end(), vTmp.begin() + AES256_KEY_SIZE, vTmp.begin() + AES256_KEY_SIZE + IV_SIZE);
+    if (!bIsTagDetached)
+    {
+      vTag.insert(vTag.end(), vTmp.begin() + AES256_KEY_SIZE + IV_SIZE, vTmp.end());
+    }
+
+  }
+  else
+  {
+    if (llFileSize <= ARGON2_METADATA_SIZE + AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE)
+    {
+      printf("Input file seems to be corrupted [%s]\n", sIn.c_str());
+      utils::CloseFile(fdIn);
+      return false;
+    }
+
+    vTmp.resize(ARGON2_METADATA_SIZE, 0);
+    llReadLimit = llFileSize - (ARGON2_METADATA_SIZE + AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE);
+    utils::SetSeekFileOffset(fdIn, llReadLimit);
+
+    if (utils::ReadFileChunk(fdIn, ARGON2_METADATA_SIZE, vTmp) != ARGON2_METADATA_SIZE)
+    {
+      printf("Error read Argon2 metadata [%s]\n", sIn.c_str());
+      utils::CloseFile(fdIn);
+      return false;
+    }
+
+    if (!bForce)
+    {
+      ucArgonVariant = vTmp[0];
+      uiArgonIterations = utils::fillUnsignedInt(std::vector<unsigned char>(vTmp.begin() + 1, vTmp.begin() + 1 + 4));
+      uiArgonThreads = utils::fillUnsignedInt(std::vector<unsigned char>(vTmp.begin() + 1 + 4, vTmp.begin() + 1 + 4 + 4));
+      ucArgonMemory = vTmp[9];
+    }
+
+    if (!bIsTagDetached)
+    {
+      if (llFileSize <= TAG_SIZE + ARGON2_METADATA_SIZE + AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE)
+      {
+        printf("Input file seems to be corrupted [%s]\n", sIn.c_str());
+        utils::CloseFile(fdIn);
+        return false;
+      }
+
+      vTmp.resize(TAG_SIZE, 0);
+      llReadLimit = llFileSize - (TAG_SIZE + ARGON2_METADATA_SIZE + AES_KEY_OPCODE_SIZE + AES_TAG_OPCODE_SIZE + AES_SIGNATURE_SIZE);
+      utils::SetSeekFileOffset(fdIn, llReadLimit);
+
+      if (utils::ReadFileChunk(fdIn, TAG_SIZE, vTmp) != TAG_SIZE)
+      {
+        printf("Error read tag metadata [%s]\n", sIn.c_str());
+        utils::CloseFile(fdIn);
+        return false;
+      }
+      vTag = vTmp;
+    }
+
+    printf("Argon2 options:");
+    printf("\n\t");
+    if (ucArgonVariant == ARGON2ID_VAR)
+    {
+      printf("Variant [Argon2id]");
+    }
+    else if (ucArgonVariant == ARGON2I_VAR)
+    {
+      printf("Variant [Argon2i]");
+    }
+    else
+    {
+      printf("Variant [Argon2d]");
+    }
+    printf("\n\t");
+    printf("Iterations [%d]", uiArgonIterations);
+    printf("\n\t");
+    printf("Threads [%d]", uiArgonThreads);
+    printf("\n\t");
+    printf("Memory [%llu KiB]\n", (unsigned long long)((unsigned long long)(1 << ucArgonMemory) * 1024) / 1024);
+
+    printf("Deriving keys...\n");
+    if (!deriveAesKeys(vPassphraseBytes, vKeyFileBytes, vKey, vIv))
+    {
+      utils::CloseFile(fdIn);
+      return false;
+    }
+  }
+
+  utils::CloseFile(fdIn);
+
+  if (bIsTagDetached && !bForce)
+  {
+    if (!utils::ReadFile(sTag, vTag))
+    {
+      printf("Could not read tag file [%s]\n", sTag.c_str());
+      return false;
+    }
+  }
+
+  printf("Decryption...\n");
+  return utils::gcmDecryptFileWrap(sIn, sOut, vKey, vIv, vTag, llReadLimit, ullProcessedBytes, bForce);
+}
+
+bool Cerberus::deriveAesKeys(const std::vector<unsigned char> &_vPassphraseBytes, const std::vector<unsigned char> &_vKeyFileBytes, std::vector<unsigned char> &_vKey, std::vector<unsigned char> &_vIv)
+{
+  if (_vPassphraseBytes.empty() && _vKeyFileBytes.empty())
+  {
+    printf("Passphrase and key file cannot be both empty\n");
+    return false;
+  }
+
+  const unsigned int uiHashLen = 16; // as recommended salt length by Argon2 spec
+  std::vector<unsigned char> vHmacHash(uiHashLen, 0);
+  std::vector<unsigned char> vKeys(AES256_KEY_SIZE + IV_SIZE, 0);
+  typedef bool FuncArgonVariant(unsigned char*, uint32_t, unsigned char*, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, unsigned char*, unsigned char*, uint32_t);
+  FuncArgonVariant *fArgonVariantPointer = NULL;
+  unsigned char* ucPointToArgonPass = _vPassphraseBytes.empty() ? (unsigned char*)_vKeyFileBytes.data() : (unsigned char*)_vPassphraseBytes.data();
+  const unsigned int uiArgonPassLen = _vPassphraseBytes.empty() ? _vKeyFileBytes.size() : _vPassphraseBytes.size();
+  unsigned char* ucOptSecret = NULL;
+  uint32_t uiOptSecretLen = 0;
+  bool bIsZeroed = false;
+
+  if (!_vPassphraseBytes.empty())
+  {
+    const unsigned int uiDivider = _vPassphraseBytes.size() > 1 ? 2 : 1;
+    utils::PBKDF2_HMAC_SHA_512(
+      (unsigned char*)_vPassphraseBytes.data(), _vPassphraseBytes.size(),
+      (unsigned char*)_vPassphraseBytes.data(), _vPassphraseBytes.size() / uiDivider,
+      PBKDF2_PRE_ITERATIONS, vHmacHash.size(), &vHmacHash[0]);
+
+    if (!_vKeyFileBytes.empty())
+    {
+      uiOptSecretLen = _vKeyFileBytes.size();
+      ucOptSecret = (unsigned char*)malloc(uiOptSecretLen);
+      if (!ucOptSecret)
+      {
+        printf("Could not allocate memory to derive keys\n");
+        return false;
+      }
+      memset(ucOptSecret, 0, uiOptSecretLen);
+
+      for (unsigned int i = 0; i < uiOptSecretLen; ++i)
+      {
+        ucOptSecret[i] = _vKeyFileBytes[i];
+      }
+    }
+  }
+  else
+  {
+    const unsigned int uiDivider = _vKeyFileBytes.size() > 1 ? 2 : 1;
+    utils::PBKDF2_HMAC_SHA_512(
+      (unsigned char*)_vKeyFileBytes.data(), _vKeyFileBytes.size(),
+      (unsigned char*)_vKeyFileBytes.data(), _vKeyFileBytes.size() / uiDivider,
+      PBKDF2_PRE_ITERATIONS, uiHashLen, &vHmacHash[0]);
+  }
+
+  if (ucArgonVariant == ARGON2ID_VAR)
+  {
+    fArgonVariantPointer = &utils::argon2id;
+  }
+  else if (ucArgonVariant == ARGON2I_VAR)
+  {
+    fArgonVariantPointer = &utils::argon2i;
+  }
+  else if (ucArgonVariant == ARGON2D_VAR)
+  {
+    fArgonVariantPointer = &utils::argon2d;
+  }
+  else
+  {
+    if (ucOptSecret)
+    {
+      utils::memset_sec(ucOptSecret, uiOptSecretLen, bIsZeroed);
+      if (!bIsZeroed)
+      {
+        printf("Memory have not been zeroed\n");
+      }
+      free(ucOptSecret);
+    }
+    printf("Unknown Argon2 variant\n");
+    return false;
+  }
+
+  if (!fArgonVariantPointer(ucPointToArgonPass, uiArgonPassLen, (unsigned char*)vHmacHash.data(), vHmacHash.size(),
+      uiArgonIterations, (1 << ucArgonMemory), uiArgonThreads, vKeys.size(), &vKeys[0], ucOptSecret, uiOptSecretLen))
+  {
+    if (ucOptSecret)
+    {
+      utils::memset_sec(ucOptSecret, uiOptSecretLen, bIsZeroed);
+      if (!bIsZeroed)
+      {
+        printf("Memory have not been zeroed\n");
+      }
+      free(ucOptSecret);
+    }
+    printf("Argon2 internal error\n");
+    return false;
+  }
+
+  if (ucOptSecret)
+  {
+    utils::memset_sec(ucOptSecret, uiOptSecretLen, bIsZeroed);
+    if (!bIsZeroed)
+    {
+      printf("Memory have not been zeroed\n");
+    }
+    free(ucOptSecret);
+  }
+
+  _vKey = (std::vector<unsigned char>(vKeys.begin(), vKeys.begin() + AES256_KEY_SIZE));
+  _vIv = (std::vector<unsigned char>(vKeys.begin() + AES256_KEY_SIZE, vKeys.end()));
+
+  return true;
+}
