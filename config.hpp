75bdc099ad0b2988635ad4c43b0780d35f522f18
kernelp4n1c <l0wentropy@proton.me>
2025-03-01 05:19:23 +0000

    release: v3.0.1 release

--- config.hpp
+++ config.hpp
@@ -1,9 +1,9 @@
 #ifndef INCLUDE_GUARD
 #define INCLUDE_GUARD
 
-#define _CERBERUS_VERSION       "3.0.0"
+#define _CERBERUS_VERSION       "3.0.1"
 #define _CERBERUS_VERSION_MAJOR "3"
 #define _CERBERUS_VERSION_MINOR "0"
-#define _CERBERUS_VERSION_PATCH "0"
+#define _CERBERUS_VERSION_PATCH "1"
 
 #endif
